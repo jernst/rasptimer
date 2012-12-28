3bf6b988212ddce84b6a93e0d771af85dc60b1a6
Johannes Ernst <Johannes.Ernst@gmail.com>
2012-12-28 15:31:09 -0800

    Factor out config file to make it usable by other people with other hardware configurations Show schedule contained in crontab, but cannot change yet

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// The devices being controlled are defined here.
+// Adjust as needed.
+
+// key: name of the device
+// value: wiringPi PIN number, see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+
+$devices = array(
+    "Main pump"    => 11,
+    "Sweeper pump" =>  6
+);

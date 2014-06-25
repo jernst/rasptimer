3b46393101efeea16bd570cef7828cb396470054
Johannes Ernst <Johannes.Ernst@gmail.com>
2014-06-25 11:27:50 -0700

    various fixes for Indie Box

--- web/emit-current-time.php
+++ web/emit-current-time.php
@@ -1,4 +1,4 @@
-<p>It currently is <?php print( date( "H:i:s" )) ?>.
+<p>It currently is <?php print( exec( "date +%H:%M:%S" )); ?>.
 <?php
 if( preg_match( '/(\d+(?:\.\d*))\s+(\d+(?:\.\d*))/', file_get_contents( '/proc/uptime' ), $matches )) {
     printf( "Up for %d days, %02d:%02d:%02d hours.", $matches[1]/(24*60*60), ($matches[1]/(60*60))%24, ($matches[1]/60)%60, $matches[1]%60 );
